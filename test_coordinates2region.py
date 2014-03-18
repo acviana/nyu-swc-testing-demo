82da545bc39c836917b6b40f04cda22a5035112b
Alex Viana <alexcostaviana@gmail.com>
2014-03-18 15:20:47 -0400

    Created my test suite.

--- test_coordinates2region.py
+++ test_coordinates2region.py
@@ -2,6 +2,13 @@
 
 """This is the nosetest module for the coordinates2region.py"""
 
-def test_foo():
-    """Bar"""
-    pass
+from coordinates2region import coordinates2region
+
+def test_coordinates2region():
+    """This function tests the functionality for the 
+    coordinates2region function."""
+    assert coordinates2region({'x':0, 'y':0}) == [1], 'Something failed.'
+    assert coordinates2region({'x':0, 'y':0}) == [1, 2], 'Something failed.'
+    assert coordinates2region({'x':0, 'y':0}) == [1], 'Something failed.'
+    assert coordinates2region({'x':0, 'y':0}) == [1], 'Something failed.'
+    assert coordinates2region({'x':0, 'y':0}) == [1], 'Something failed.'
